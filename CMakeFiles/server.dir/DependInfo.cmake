39e641575c6dcfabd05c1887282fc925acfdcec9
omcal <83889679+omcal@users.noreply.github.com>
2024-04-16 12:39:32 +0300

    Add files via upload

---
+++ CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/joi/Workdir/codecrafts/codecrafters-git-cpp/src/Error.cpp" "CMakeFiles/server.dir/src/Error.cpp.o" "gcc" "CMakeFiles/server.dir/src/Error.cpp.o.d"
+  "/home/joi/Workdir/codecrafts/codecrafters-git-cpp/src/Server.cpp" "CMakeFiles/server.dir/src/Server.cpp.o" "gcc" "CMakeFiles/server.dir/src/Server.cpp.o.d"
+  "/home/joi/Workdir/codecrafts/codecrafters-git-cpp/src/zlib_implement.cpp" "CMakeFiles/server.dir/src/zlib_implement.cpp.o" "gcc" "CMakeFiles/server.dir/src/zlib_implement.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/src/Error.cpp.o"
+  "CMakeFiles/server.dir/src/Error.cpp.o.d"
+  "CMakeFiles/server.dir/src/Server.cpp.o"
+  "CMakeFiles/server.dir/src/Server.cpp.o.d"
+  "CMakeFiles/server.dir/src/zlib_implement.cpp.o"
+  "CMakeFiles/server.dir/src/zlib_implement.cpp.o.d"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
